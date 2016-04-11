2bef3bc8b1d75a1e142803c41e7869c0e9276a71
student1 <student1@jh-mbair-158-06.d214.org>
2016-04-11 10:15:53 -0500

    continued work on hack which

--- MobileMapper/ViewController.swift
+++ MobileMapper/ViewController.swift
@@ -9,13 +9,14 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,MKMapViewDelegate {
+    
     @IBOutlet weak var myMapView: MKMapView!
     let ord = MKPointAnnotation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        myMapView.delegate = self
         let latitude = 41.9742
         let longitude = -87.9073
         let coordinate = CLLocationCoordinate2DMake(latitude, longitude)
@@ -23,14 +24,36 @@ class ViewController: UIViewController {
         ord.coordinate = coordinate
         ord.title = "O'Hare Airport"
         myMapView.addAnnotation(ord)
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+        let address = "Mount Rushmore"
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(address) { (arrayOfPlacemarks, error) -> Void in
+            for place in arrayOfPlacemarks!
+            {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = (place.location?.coordinate)!
+                annotation.title = place.name
+                self.myMapView.addAnnotation(annotation)
+            }
+        }
+        //let address2 = "Los Angeles"
+        //let geoCoder2 = CLGeocoder()
+        //geoCoder.geocodeAddressString(address2) { (arrayOfPlacemarks2, error) -> Void in
+          //  for place2 in arrayOfPlacemarks2!
+            //{
+              //  let annotation2 = MKPointAnnotation()
+                //annotation2.coordinate = (place2.location?.coordinate)!
+                //annotation2.title = place2.name
+                //self.myMapView.addAnnotation(annotation2)
+            //}
+            
+        //}
+        func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+            let pin = MKAnnotationView(annotation: annotation, reuseIdentifier:  nil)
+            pin.image = UIImage(named: "mobilemakers")
+            pin.canShowCallout = true
+            let button = UIButton(type: .DetailDisclosure)
+            pin.rightCalloutAccessoryView = button
+            return pin
+        }
 }
-
+}
